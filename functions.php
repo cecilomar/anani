7aa188504fb75c3af3adec1f000683fa6f225a30
Cecil O. Almonte <info@cecilomar.com>
2014-12-28 12:20:56 -0500

    Update functions.php
    Added branding and a test function.

--- functions.php
+++ functions.php
@@ -1,3 +1,23 @@
 <?php
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// @package    	Ananí                                                                             //
+// @author		Cecil O. Almonte                                                                  //
+// @website		https://github.com/cecilomar/anani                                                //
+// @copyright	Copyright (C) Cecilomar Design, Inc. 2014                                         //
+// @license		http://www.gnu.org/licenses/gpl.txt                                               //
+// @description	Ananí is a generic API infrastructure written in PHP.                             //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// This is where you should write or include your user defined functions.                         //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Test Function.
+function test($arg){
+	if($arg){
+		$output = date($arg);
+	} else{
+		$output = date('U');
+	}
+	return $output;
+}
 
 ?>
